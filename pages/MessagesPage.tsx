b5b60923cc9b8e766e422acf089c525f26054aab
Prince Gupta <beatslevelone@gmail.com>
2026-02-24 12:25:57 +0530

    Update MessagesPage.tsx
    fix

--- pages/MessagesPage.tsx
+++ pages/MessagesPage.tsx
@@ -526,7 +526,7 @@ messagesEndRef.current?.scrollIntoView({ behavior: 'auto' });
     </aside>
 
     {/* ================= CHAT AREA ================= */}
-    <main className={`flex-1 flex flex-col h-full ${selectedChatId ? 'flex' : 'hidden md:flex'}`}>
+    <main className={`flex-1 flex flex-col min-h-0 ${selectedChatId ? 'flex' : 'hidden md:flex'}`}>
 
 {selectedChat ? (
   <>
