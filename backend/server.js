c839913f921a0e31dd8ba07765f28fca336e8fa0
Prince Gupta <beatslevelone@gmail.com>
2026-02-25 21:15:04 +0530

    Update server.js
    wait

--- backend/server.js
+++ backend/server.js
@@ -70,3 +70,7 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
+
+app.get('/test123', (req,res)=>{
+  res.send("TEST OK")
+})
