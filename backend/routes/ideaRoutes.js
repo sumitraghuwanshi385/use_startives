b63dd1e023bffd2c08452fa09338a31bd436a2e9
Prince Gupta <beatslevelone@gmail.com>
2026-02-25 20:56:15 +0530

    Update ideaRoutes.js
    fixed

--- backend/routes/ideaRoutes.js
+++ backend/routes/ideaRoutes.js
@@ -1,15 +1,11 @@
-// backend/routes/ideaRoutes.js
 const express = require('express');
-const { createIdea, getIdeas, getIdeaById } = require('../controllers/ideaController');
+const ideaController = require('../controllers/ideaController');
 const { protect } = require('../middleware/authMiddleware');
 
 const router = express.Router();
 
-// GET all ideas
-// POST create idea
-router.route('/').get(getIdeas).post(protect, createIdea);
-
-// GET single idea
-router.get('/:id', getIdeaById);
+router.get('/', ideaController.getIdeas);
+router.post('/', protect, ideaController.createIdea);
+router.get('/:id', ideaController.getIdeaById);
 
 module.exports = router;
