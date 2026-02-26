74804749ceda9b5e03fb3bf45dc1a6552eeb13b4
Prince Gupta <beatslevelone@gmail.com>
2026-02-26 17:29:52 +0530

    Update MyApplicationsPage.tsx
    fixed

--- pages/MyApplicationsPage.tsx
+++ pages/MyApplicationsPage.tsx
@@ -164,7 +164,7 @@ const SentCard: React.FC<{ application: Application; idea?: StartupIdea }> = ({
             <Link to={`/user/${founder.id}`}>
               <img
                 src={founder.profilePictureUrl}
-                className="w-8 h-8 rounded-full object-cover border border-[var(--border-secondary)]"
+                className="w-9 h-9 rounded-full object-cover border border-[var(--border-secondary)]"
               />
             </Link>
 
