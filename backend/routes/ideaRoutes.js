bb87d99207856c723abfb882f82fc075433ede4f
Prince Gupta <beatslevelone@gmail.com>
2026-02-28 01:02:41 +0530

    Update ideaRoutes.js
    fixed

--- backend/routes/ideaRoutes.js
+++ backend/routes/ideaRoutes.js
@@ -8,4 +8,8 @@ router.get('/', ideaController.getIdeas);
 router.post('/', protect, ideaController.createIdea);
 router.get('/:id', ideaController.getIdeaById);
 
+// ✅ ADD THESE TWO
+router.put('/:id', protect, ideaController.updateIdea);
+router.delete('/:id', protect, ideaController.deleteIdea);
+
 module.exports = router;
