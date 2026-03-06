b6094be53458de74f3d2259fa66bf6d07d9276ec
Prince Gupta <beatslevelone@gmail.com>
2026-03-06 22:29:49 +0530

    Update TeamDetailPage.tsx

--- pages/TeamDetailPage.tsx
+++ pages/TeamDetailPage.tsx
@@ -4,7 +4,7 @@ import { useAppContext } from '../contexts/AppContext';
 import { ChatConversation, User, DetailedMessage } from '../types';
 import { ChevronLeftIcon } from '../constants';
 
-// --- Icons ---
+// --- Icons --- 
 const PencilIcon: React.FC<{ className?: string }> = ({ className = "w-5 h-5" }) => (
   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={className}><path strokeLinecap="round" strokeLinejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125" /></svg>
 );
