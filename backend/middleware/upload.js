011353d33969ea41ade575c9e693dafde839fc54
Prince Gupta <beatslevelone@gmail.com>
2026-03-09 19:54:37 +0530

    Update upload.js

--- backend/middleware/upload.js
+++ backend/middleware/upload.js
@@ -1,7 +1,6 @@
-import multer from "multer";
-import { CloudinaryStorage } from "multer-storage-cloudinary";
-import cloudinary from "../config/cloudinary.js";
-
+const multer = require("multer");
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
+const cloudinary = require("../config/cloudinary");
 const allowedMimeTypes = [
 
 "image/jpeg",
