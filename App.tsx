6dbb2018e6e18d5f90f170a30d0297f615244863
Prince Gupta <beatslevelone@gmail.com>
2026-04-21 16:30:11 +0530

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -39,6 +39,7 @@ import ContactUsPage from './pages/ContactUsPage';
 import SearchPage from './pages/SearchPage';
 import { useAppContext } from './contexts/AppContext';
 import FloatingActionMenu from './components/FloatingActionMenu';
+import GlobalGlobe from './pages/GlobalGlobe';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 
 
@@ -156,6 +157,7 @@ useEffect(() => {
           <Route path="/new-password" element={<NewPasswordPage />} />
           <Route path="/contact-us" element={<WithPageContainer><ContactUsPage /></WithPageContainer>} />
           <Route path="/search" element={<WithPageContainer><SearchPage /></WithPageContainer>} />
+<Route path="/globe" element={<GlobalGlobe />} />
 
           <Route path="/dashboard" element={
   <ProtectedRoute>
