f6bdc5811d32e4d633016ac48f02c43a39812e07
Prince Gupta <beatslevelone@gmail.com>
2026-05-28 19:05:43 +0530

    Update stories.ts

--- data/stories.ts
+++ data/stories.ts
@@ -2,10 +2,12 @@ import { photoAIStory } from "./stories/photoAIStory";
 import { nomadListStory } from "./stories/nomadListStory";
 import { beehiivStory } from "./stories/beehiivStory";
 import { levelsFYIStory } from "./stories/levelsFYIStory";
+import { carrdStory } from "./stories/carrdStory";
 
 export const stories = [
   photoAIStory,
   nomadListStory,
   beehiivStory,
   levelsFYIStory,
+carrdStory,
 ];
